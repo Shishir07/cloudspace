18cd14a5019ddbfb939af1204ece3ea49db92ad0
Shishir Sandeep <shishir.s@iblmitstock.local>
2020-10-06 19:31:51 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, {useState, useEffect, useContext} from 'react';
+import {BrowserRouter as Router,
+    Switch,
+    Route,
+    Link} from 'react-router-dom';
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+
+function App( props) {
+  const [state, changeState] = useState({
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  });
+
+  const handleIncrement = counter => {
+    const counters = [state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value++;
+    changeState({ counters });
+  };
+
+  const handleDecrement = counter => {
+    const counters = [state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counters[index] };
+    counters[index].value--;
+    changeState({ counters });
+  };
+
+  const handleReset = () => {
+    const counters = state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    changeState({ counters });
+  };
+
+  const handleDelete = counterId => {
+    const counters = state.counters.filter(c => c.id !== counterId);
+    changeState({ counters });
+  };
+
+  const handleRestart = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div>
+      <NavBar
+        totalCounters={state.counters.filter(c => c.value > 0).length}
+      />
+      <div className="ompanel">
+        <NavBar/>
+        <div className="containerbody">
+          <Counters
+              counters={state.counters}
+              onReset={handleReset}
+              onIncrement={handleIncrement}
+              onDecrement={handleDecrement}
+              onDelete={handleDelete}
+              onRestart={handleRestart}
+          />
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
