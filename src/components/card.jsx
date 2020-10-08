0a2adaa097850ddbd6f94259b069b8b4cb4a4bd7
subhranil.m <subhranil.m@endurance.com>
2020-10-08 17:04:58 +0530

    modified css and added image for nginx instance

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import NavBar2 from "../components/navbar2"
+import NginxLogo from "./nginxlogo.png";
 
 
 // Stateless Functional Component
 
 const Card = ({serverDetails}) => {
     return (
-            <div className="card">
+            <div className="card" style={{backgroundColor: "lightblue"}}>
                 <div className="container-new">
-                    <h4><b>{serverDetails.instanceName}</b></h4>
-                    <p>{serverDetails.userId}</p>
+                    <div><img className={"service-icon"} src={NginxLogo}/></div>
+                    <div className="instance-name"><h4><b>Instance Name : {serverDetails.instanceName}</b></h4></div>
+                    <div>Username : {serverDetails.userId}</div>
+                    <div><button type="submit" >Start</button> <button type="submit" >Stop</button></div>
                 </div>
             </div>
     );
